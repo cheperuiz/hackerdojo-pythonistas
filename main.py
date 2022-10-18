2789220b8bc06a405dba1ef54e4a3c8f97c55b7c
Chepe Ruiz <chepe@chepe-mbp.local>
2022-10-18 16:59:28 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+import asyncio
+import aiohttp
+import cv2
+from openvino.runtime import Core
+import io
+import numpy as np
+
+app = FastAPI()
+
+ie = Core()
+model = ie.read_model(model="model/v3-small_224_1.0_float.xml")
+compiled_model = ie.compile_model(model=model, device_name="MYRIAD")
+
+output_layer = compiled_model.output(0)
+
+
+async def download_image(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.read()
+            arr = np.fromstring(data, np.uint8)
+            return cv2.imdecode(arr, cv2.IMREAD_COLOR)
+
+
+def preprocess_image(src):
+    image = cv2.cvtColor(src, code=cv2.COLOR_BGR2RGB)
+    input_image = cv2.resize(src=image, dsize=(224, 224))
+    return np.expand_dims(input_image, 0)
+
+
+def infer_on(input_image):
+
+    result_infer = compiled_model([input_image])[output_layer]
+    result_index = np.argmax(result_infer)
+    # Convert the inference result to a class name.
+    imagenet_classes = open("utils/imagenet_2012.txt").read().splitlines()
+
+    # The model description states that for this model, class 0 is a background.
+    # Therefore, a background must be added at the beginning of imagenet_classes.
+    imagenet_classes = ["background"] + imagenet_classes
+
+    return imagenet_classes[result_index]
+
+
+@app.get("/")
+async def root():
+    original = await download_image(
+        "https://www.thesprucepets.com/thmb/bFYTnEGrEtOXpG5PWj9vKXhzGhI=/3864x2576/filters:fill(auto,1)/GettyImages-491785001-0b67be9de07442b980ad682ac934c573.jpg"
+    )
+    input_image = preprocess_image(original)
+    return infer_on(input_image)
